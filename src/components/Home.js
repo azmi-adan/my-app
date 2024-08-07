8dd26a307177420a10019f46fc3a72ec1ada9b3a
azmi adan <azmi.adan2032@gmail.com>
2024-08-07 11:30:29 +0300

    add all

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// src/Home.js
+import React from 'react';
+
+function Home() {
+  return(
+    <div>
+   <h1>Welcome to the home page!</h1>;
+  <p>this is how it works</p>
+  </div>
+  )
+}
+
+export default Home;
